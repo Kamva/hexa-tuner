c14d1be20b9d4bdae159c86486b77657a624eb7d
Mehran Prs <mehr.prs@gmail.com>
2020-02-19 03:04:19 +0330

    Add log

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/Kamva/elogrus/v4 v4.0.0 // indirect
 	github.com/Kamva/gutil v0.0.0-20200218195324-25eb19867a90 // indirect
-	github.com/spf13/viper v1.6.2 // indirect
+	github.com/Kamva/kitty v0.0.0-20200218211952-1c4d123f20a7 // indirect
+	github.com/Kamva/logrus-kit v0.0.0-20200218181028-1d56103db75d // indirect
 )
---
+++ log.go
@@ -0,0 +1,18 @@
+package ktuner
+
+import (
+	"github.com/Kamva/kitty"
+	"github.com/Kamva/logrus-kit"
+	"github.com/sirupsen/logrus"
+)
+
+// NewLogger return new instance of logger.
+func NewLogger(config kitty.Config) (*logrus.Logger, error) {
+	logger := logrus.New()
+
+	if err := logruskit.Tune(logger, logruskit.Config(config)); err != nil {
+		return nil, err
+	}
+
+	return logger, nil
+}
