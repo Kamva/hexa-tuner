64f5ad872e92b15a9dd898475c0a2ccdd9ea5f4f
Mehran Prs <mehr.prs@gmail.com>
2020-02-19 17:13:31 +0330

    Change NewLogger function to return newinstance of kitty logger.

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/Kamva/elogrus/v4 v4.0.0 // indirect
-	github.com/Kamva/kitty v0.0.0-20200219113856-b4f3f14ac26a // indirect
+	github.com/Kamva/kitty v0.0.0-20200219133919-60d10f66ec4f // indirect
 	github.com/Kamva/logrus-kit v0.0.0-20200218181028-1d56103db75d // indirect
 )
--- log.go
+++ log.go
@@ -2,18 +2,19 @@ package ktuner
 
 import (
 	"github.com/Kamva/kitty"
+	"github.com/Kamva/kitty/kittylogger"
 	"github.com/Kamva/logrus-kit"
 	_ "github.com/Kamva/logrus-kit/logrusbase"
 	"github.com/sirupsen/logrus"
 )
 
-// NewLogger return new instance of logger.
-func NewLogger(config kitty.Config) (*logrus.Logger, error) {
+// NewLogger return new instance of kitty logger service.
+func NewLogger(config kitty.Config) (kitty.Logger, error) {
 	logger := logrus.New()
 
 	if err := logruskit.Tune(logger, logruskit.Config(config)); err != nil {
 		return nil, err
 	}
 
-	return logger, nil
+	return kittylogger.NewLogrusDriver(logger.WithFields(nil)), nil
 }
