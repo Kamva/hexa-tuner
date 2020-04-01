cf61110c6dc14b0d652f86592327fa0e527c419a
Mehran Prs <mehr.prs@gmail.com>
2020-04-01 16:28:46 +0430

    Update to new hexa jobs

--- go.mod
+++ go.mod
@@ -5,9 +5,9 @@ go 1.13
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/Kamva/gutil v0.0.0-20200329114904-c3f6b07357cc
-	github.com/Kamva/hexa v0.0.0-20200401104712-c4b4a2760c92
+	github.com/Kamva/hexa v0.0.0-20200401111121-8d5bd989f5d2
 	github.com/Kamva/hexa-echo v0.0.0-20200401112024-6f3e7dcdfbe0
-	github.com/Kamva/hexa-job v0.0.0-20200308121121-5ba1aabd3d84
+	github.com/Kamva/hexa-job v0.0.0-20200401115611-361cab934429
 	github.com/Kamva/logrus-kit v0.0.0-20200218181028-1d56103db75d
 	github.com/Kamva/tracer v0.0.0-20200227202933-3d5974771f25
 	github.com/contribsys/faktory v1.2.0-1
--- jobs.go
+++ jobs.go
@@ -10,20 +10,20 @@ import (
 )
 
 // NewFaktoryJobsDriver generate new faktory driver for hexa jobs.
-func NewFaktoryJobsDriver(poolSize int) (hjob.Jobs, error) {
+func NewFaktoryJobsDriver(ctxExporterImporter hexa.ContextExporterImporter, poolSize int) (hjob.Jobs, error) {
 	p, err := faktory.NewPool(poolSize)
 
 	if err != nil {
 		return nil, tracer.Trace(err)
 	}
 
-	return hexafaktory.NewFaktoryJobsDriver(p), nil
+	return hexafaktory.NewFaktoryJobsDriver(p, ctxExporterImporter), nil
 }
 
 // NewFaktoryWorkerDriver generate new faktory driver for the hexa worker.
-func NewFaktoryWorkersDriver(uf hexa.UserFinder, concurrency int, l hexa.Logger, t hexa.Translator) (hjob.Worker, error) {
+func NewFaktoryWorkersDriver(ctxExporterImporter hexa.ContextExporterImporter, concurrency int) (hjob.Worker, error) {
 	mgr := faktoryworker.NewManager()
-	worker := hexafaktory.NewFaktoryWorkerDriver(mgr, uf, l, t)
+	worker := hexafaktory.NewFaktoryWorkerDriver(mgr, ctxExporterImporter)
 	err := worker.Concurrency(concurrency)
 
 	return worker, tracer.Trace(err)
