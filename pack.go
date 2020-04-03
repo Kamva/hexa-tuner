5d82d300ac90cfc910695ed4b7e1a7bd104e5950
Mehran Prs <mehr.prs@gmail.com>
2020-04-03 17:51:31 +0430

    Add Gate servcie to the base pack

--- pack.go
+++ pack.go
@@ -12,19 +12,20 @@ var (
 	errNilLogger     = errors.New("logger is Nil in the pack")
 	errNilTranslator = errors.New("translator is Nil in the pack")
 	errNilJobs       = errors.New("jobs is nil in the pack")
+	errNilGate       = errors.New("gate is nil in the pack")
 )
 
 // Pack contains all of services in one place to manage our services.
 type Pack struct {
-	// must specify that should panic if one service is
-	//nil and user request to get that service or just
-	//returns nil.
+	// must specify that should panic when user request
+	// to get a nil service or just return nil value.
 	must bool
 
 	config     hexa.Config
 	log        hexa.Logger
 	translator hexa.Translator
 	jobs       hjob.Jobs
+	gate       hexa.Gate
 }
 
 // SetConfig sets the config service.
@@ -47,32 +48,53 @@ func (p *Pack) SetJobs(jobs hjob.Jobs) {
 	p.jobs = jobs
 }
 
+// SetGate sets the Gate service.
+func (p *Pack) SetGate(gate hexa.Gate) {
+	p.gate = gate
+}
+
 // Config returns the config service.
 func (p *Pack) Config() hexa.Config {
-	gutil.PanicNil(p.config, errNilConfig)
+	if p.must {
+		gutil.PanicNil(p.config, errNilConfig)
+	}
 
 	return p.config
 }
 
 // Logger returns the logger service.
 func (p *Pack) Logger() hexa.Logger {
-	gutil.PanicNil(p.log, errNilLogger)
+	if p.must {
+		gutil.PanicNil(p.log, errNilLogger)
+	}
 
 	return p.log
 }
 
 // Translator returns the translator service.
 func (p *Pack) Translator() hexa.Translator {
-	gutil.PanicNil(p.translator, errNilTranslator)
-
+	if p.must {
+		gutil.PanicNil(p.translator, errNilTranslator)
+	}
 	return p.translator
 }
 
 // Jobs returns the jobs service.
 func (p *Pack) Jobs() hjob.Jobs {
-	gutil.PanicNil(p.jobs, errNilJobs)
+	if p.must {
+		gutil.PanicNil(p.jobs, errNilJobs)
+	}
 
 	return p.jobs
+}
+
+// Gate returns the gate service.
+func (p *Pack) Gate() hexa.Gate {
+	if p.must {
+		gutil.PanicNil(p.gate, errNilGate)
+	}
+
+	return p.gate
 }
 
 // NewPack returns new instance of the pack.
