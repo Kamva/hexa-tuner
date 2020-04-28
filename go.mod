d5c4ca60ec5eaac39f50864b6e4338ed86aa154f
Mehran Prs <mehr.prs@gmail.com>
2020-04-28 22:41:46 +0430

    Add error interceptor to gRPC server and client interceptors

--- go.mod
+++ go.mod
@@ -5,15 +5,16 @@ go 1.13
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/Kamva/gutil v0.0.0-20200410105054-7e8534aaefbe
-	github.com/Kamva/hexa v0.0.0-20200424113838-faee1df645cf
+	github.com/Kamva/hexa v0.0.0-20200428175836-8c1507eacf26
 	github.com/Kamva/hexa-echo v0.0.0-20200423182552-737413bd7a13
 	github.com/Kamva/hexa-event v0.0.0-20200403172840-5ab3358c1d8b
 	github.com/Kamva/hexa-job v0.0.0-20200401115611-361cab934429
-	github.com/Kamva/hexa-rpc v0.0.0-20200428074026-f8ab2327d751 // indirect
+	github.com/Kamva/hexa-rpc v0.0.0-20200428180723-264b8259fb5b
 	github.com/Kamva/logrus-kit v0.0.0-20200218181028-1d56103db75d
 	github.com/Kamva/tracer v0.0.0-20200410105359-fd5aa0f880a8
 	github.com/contribsys/faktory v1.2.0-1
 	github.com/contribsys/faktory_worker_go v1.0.2-0.20200224171227-14a9992ad00e
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
 	github.com/labstack/echo/v4 v4.1.14
 	github.com/nicksnyder/go-i18n/v2 v2.0.3
 	github.com/sirupsen/logrus v1.4.2
--- grpc.go
+++ grpc.go
@@ -8,27 +8,42 @@ import (
 	"google.golang.org/grpc/grpclog"
 )
 
+type GRPCServerTunerOptions struct {
+	cei hexa.ContextExporterImporter
+	cfg hexa.Config
+	l   hexa.Logger
+	t   hexa.Translator
+}
+
 // GRPCConn returns new instance of the gRPC connection with your config to use in client
 func GRPCConn(serverAddr string, cei hexa.ContextExporterImporter) (*grpc.ClientConn, error) {
-	hexaCtxInt := hrpc.NewHexaContextInterceptor(cei)
-	unaryInt := grpc.WithUnaryInterceptor(hexaCtxInt.UnaryClientInterceptor)
+	unaryInt := grpc.WithChainUnaryInterceptor(
+		// Hexa error interceptor (convert gRPC status to hexa error)
+		hrpc.NewErrorInterceptor().UnaryClientInterceptor(),
+		// Hexa context interceptor
+		hrpc.NewHexaContextInterceptor(cei).UnaryClientInterceptor,
+	)
 	// TODO: Init metric API and distributed tracing here.
 
 	return grpc.Dial(serverAddr, grpc.WithInsecure(), unaryInt)
 }
 
 // TuneGRPCServer returns new instance of the tuned gRPC Server to server requests to services
-func TuneGRPCServer(cei hexa.ContextExporterImporter, cfg hexa.Config, l hexa.Logger) (*grpc.Server, error) {
-	loggerOptions := hrpc.DefaultLoggerOptions(cfg.GetBool("DEBUG"))
+func TuneGRPCServer(o GRPCServerTunerOptions) (*grpc.Server, error) {
+	loggerOptions := hrpc.DefaultLoggerOptions(o.cfg.GetBool("DEBUG"))
 
 	// Replace gRPC logger with hexa logger
-	grpclog.SetLoggerV2(hrpc.NewLogger(l, cfg))
+	grpclog.SetLoggerV2(hrpc.NewLogger(o.l, o.cfg))
 
 	intChain := grpc_middleware.ChainUnaryServer(
-		// Set Hexa context interceptor
-		hrpc.NewHexaContextInterceptor(cei).UnaryServerInterceptor,
-		// Set request logger
-		hrpc.NewRequestLogger(l).UnaryServerInterceptor(loggerOptions),
+		// Hexa context interceptor
+		hrpc.NewHexaContextInterceptor(o.cei).UnaryServerInterceptor,
+
+		// Request logger
+		hrpc.NewRequestLogger(o.l).UnaryServerInterceptor(loggerOptions),
+
+		// Hexa error interceptor (Must be last interceptor)
+		hrpc.NewErrorInterceptor().UnaryServerInterceptor(o.t),
 	)
 	return grpc.NewServer(grpc.UnaryInterceptor(intChain)), nil
 }
